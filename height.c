9f7c390b6243192cafb89e74cc22215f313775e9
Rayner <raynersulyak@gmail.com>
2018-11-12 19:30:24 -0200

    general improvements

--- height.c
+++ height.c
@@ -4,6 +4,7 @@
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
 int height(struct node *arv);
+int height_alt(struct node *arv);
 
 int main(){
     struct node *arv;
@@ -11,9 +12,11 @@ int main(){
     infix(arv);
     printf("\n");
     printf("altura: %d\n", height(arv));
+    printf("altura: %d\n", height_alt(arv));
 }
 
 int height(struct node *arv){
+    //"a altura de uma arvore eh definida por: 'a maior altura das suas subarvores mais 1(sua raiz)'"
     int altura_aux(struct node *arv){
         if(arv != (struct node *)NULL){
             int a = altura_aux(arv->left);
@@ -24,4 +27,16 @@ int height(struct node *arv){
             return 0;
     }
     return altura_aux(arv) - 1;
+}
+
+//maneira alternativa de calcular altura
+int height_alt(struct node *arv){
+    if(arv != (struct node *)NULL){
+        int a = height_alt(arv->left);
+        int b = height_alt(arv->right);
+        return max(a, b) + 1;
+    }
+    //height of a non-existent tree could be considered -1 for recursion purpuses
+    else
+        return -1; //will only be added once to the arithmetics
 }
